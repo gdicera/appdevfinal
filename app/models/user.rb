abf594b3f88a8bd0885bf40defc4579b64368491
gdicera <giuseppedicera@uchicago.edu>
2023-03-02 04:08:42 +0000

    added associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,11 @@ class User < ApplicationRecord
   validates :email, :presence => true
   has_secure_password
 
+  has_many(:liked_posts, { :class_name => "LikedPost", :foreign_key => "user_id" })
+  has_many(:posts, { :class_name => "Post", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:topics, { :class_name => "Topic", :foreign_key => "user_id" })
+  has_many(:favorite_posts, { :through => :liked_posts, :source => :post })
+
+
   mount_uploader :image, ImageUploader
 end
