0b44c780bcfc0dde633556303fe6dbf1ec1782ca
gdicera <giuseppedicera@uchicago.edu>
2023-03-03 20:49:06 +0000

    got posts to populate on topics page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,19 +14,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    the_topic = Topic.new
-    the_topic.user_id = session.fetch(:user_id)
-    
     the_post = Post.new
+
+    the_post.topic_id = params.fetch("query_topic_id")
     the_post.user_id = session.fetch(:user_id)
-    #the_post.the_topic.id = params.fetch(:topic_id)
     the_post.body = params.fetch("input_body")
 
     if the_post.valid?
       the_post.save
-      redirect_to("/topics/#{the_topic.id}", { :notice => "Post created successfully." })
+      redirect_to("/topics/#{the_post.topic_id}", { :notice => "Post created successfully." })
     else
-      redirect_to("/topics/#{the_topic.id}", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/topics/#{the_post.topic_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
@@ -34,15 +32,15 @@ class PostsController < ApplicationController
     the_id = params.fetch("path_id")
     the_post = Post.where({ :id => the_id }).at(0)
 
-    the_post.user_id = params.fetch("query_user_id")
-    #the_post.topic_id = params.fetch("query_topic_id")
+    the_post.topic_id = params.fetch("query_topic_id")
+    the_post.user_id = session.fetch(:user_id)
     the_post.body = params.fetch("input_body")
 
     if the_post.valid?
       the_post.save
-      redirect_to("/topics/#{the_topic.id}", { :notice => "Post updated successfully."} )
+      redirect_to("/topics/#{the_post.topic_id}", { :notice => "Post updated successfully."} )
     else
-      redirect_to("/topics/#{the_topic.id}", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/topics/#{the_post.topic_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
